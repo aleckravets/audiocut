7498e7be6fc5aa251060f892a801b57059d81c7b
akravets <aleckravets@gmail.com>
2025-01-29 16:10:40 +0300

    adding gradient

--- src/drawWaveform.ts
+++ src/drawWaveform.ts
@@ -26,7 +26,8 @@ export function drawWaveform(
 
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  context.fillStyle = '#fff';
+  // context.fillStyle = '#fff';
+  context.fillStyle = canvasLinearGradient(context, waveGradient);
 
   for (let i = 0; i < scaledData.length; i++) {
     const barHeight = scaledData[i];
@@ -37,3 +38,26 @@ export function drawWaveform(
 
   context.clearRect(0, canvas.height * lineOffset, canvas.width, 1);
 }
+
+function canvasLinearGradient(canvasCtx: any, colorStops: any) {
+
+  var l = canvasCtx.canvas.height; // Gradient Size
+
+  var u = colorStops.map(function (stop: any) {
+    var opacity = stop.length === 3 ? stop[2] : 1;
+
+    return [stop[0], stop[1], opacity]
+  })
+
+  return u.reduce(function (e: any, stop: any) {
+    var sValue = stop[0],
+      color = stop[1],
+      opacity = stop[2],
+      clr = "rgba(" + color + "," + opacity + ")";
+
+    return e.addColorStop(sValue, clr), e;
+
+  }, canvasCtx.createLinearGradient(0, 0, 0, l))
+}
+
+const waveGradient = [[0, "255,255,255"], [0.7, "255,255,255"], [0.701, "229,229,229"], [1, "229,229,229"]];
